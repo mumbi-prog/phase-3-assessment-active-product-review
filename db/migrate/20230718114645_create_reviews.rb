d09cc59108862a91a5d5cb1689e6f7438e1fb6cd
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-18 12:17:48 +0300

    Completed

---
+++ db/migrate/20230718114645_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+    def change
+      create_table :reviews do |t|
+        t.integer :star_rating
+        t.text :comment
+        t.references :product, foreign_key: true
+        t.references :user, foreign_key: true
+  
+        t.timestamps
+      end
+    end
+end
+  
