bc6748524af1c8ba7b815202be79332ca5ed26b7
Sylvia Mumbi <mumbi099@gmail.com>
2023-07-20 04:44:50 +0300

    Updated files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,20 @@
 class Product < ActiveRecord::Base
     
-    has_many :reviews
-    has_many :users, through: :reviews
-  
-    def leave_review(user, star_rating, comment)
-      Review.create(user_id: user.id, product_id: id, star_rating: star_rating, comment: comment)
+  has_many :reviews
+  has_many :users, through: :reviews
+
+  def leave_review(user_attributes, star_rating, comment)
+    user = User.find_or_create_by(user_attributes)
+    Review.create(user: user, product: self, star_rating: star_rating, comment: comment)
+  end
+
+  def print_all_reviews
+    reviews.each do |review|
+      puts "Review for #{name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
     end
-  
-    def print_all_reviews
-      reviews.each do |review|
-        puts "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
-      end
-    end
-  
-    def average_rating
-      reviews.average(:star_rating).to_f
-    end
-    
-end 
-  
+  end
+
+  def average_rating
+    reviews.average(:star_rating).to_f
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ActiveRecord::Base
-    has_many :reviews
-    has_many :products, through: :reviews
-  
-    def favorite_product
-      products.order(:reviews).last
-    end
-  
-    def remove_reviews(product)
-      reviews.where(product_id: product.id).destroy_all
-    end
+    
+  has_many :reviews
+  has_many :products, through: :reviews
+
+  def favorite_product
+    products.order('reviews.star_rating DESC').first
+  end
+
+  def remove_reviews(product)
+    reviews.where(product_id: product.id).destroy_all
+  end
 end
