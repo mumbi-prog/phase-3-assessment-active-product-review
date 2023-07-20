7e7ba0267f33fd33a1dbbbc074c1aeaa84a67e31
mumbi <mumbi099@gmail.com>
2023-07-20 04:47:25 +0300

    Updated files

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,11 @@
 class Review < ActiveRecord::Base
-
+    
     belongs_to :product
     belongs_to :user
   
     def print_review
-        review_block = "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
-        puts review_block
+      puts "Review for #{product.name} by #{user.name}: #{star_rating}. #{comment}"
     end
-
 end
+
   
